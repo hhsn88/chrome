1cbd991eaf99cf85491a0f262c63972f7a496c6a
hsn <hhsn88@gmail.com>
2019-07-03 21:51:17 +0300

    [prog-trkr] Fixed list sorting & improved styling

--- prog-tracker/readingList.js
+++ prog-tracker/readingList.js
@@ -3,7 +3,7 @@ $(function () {
         chrome.storage.sync.get(null, (data) => {
             if (data) {
                 var keys = Object.keys(data).filter( (key) => key.startsWith('ProgTrkr_'))
-                                            .sort( (k1, k2) => (new Date(data[k1].time) - new Date(data[k2].time)) > 0);
+                                            .sort( (k1, k2) => (new Date(data[k1].time) - new Date(data[k2].time)));
                 for (let key of keys) {
                     const url = key.substr('ProgTrkr_'.length) + '?prg_trkr=' + data[key].scroll;
                     $('#sitesList').prepend(
@@ -47,15 +47,15 @@ $(function () {
     const getTemplate = (title, url, time) => {
         var t = new Date(time);
         var urlDomain = extractDomain(url);
-        return `<div style="display: flex; flex-direction: column; word-break: break-all; padding-left: 10px; padding-top: 8px; position: relative; font-size: 13px;">
+        return `<div style="display: flex; flex-direction: column; word-break: break-all; padding: 0.5%; position: relative; font-size: 1.2em;">
                     <div style="width: 50%">
-                        <img class="cross" src="images/cross.png" style="visibility: hidden; top: 10px; left: 0px; width: 14px; height: 13px; vertical-align: middle; padding-top: 2px; cursor: pointer;">
-                        <img src="https://www.google.com/s2/favicons?domain=${urlDomain}" style="top: 0px; left: 25px; width: 16px; height: 16px; vertical-align: middle;">
-                        <a href="${url}" target="_blank" style="padding-right: 12px; text-decoration: none; cursor: pointer; color: #234da7;">
-                            ${title}&nbsp
+                        <img class="cross" src="images/cross.png" style="visibility: hidden; width: 1rem; vertical-align: middle; cursor: pointer;">
+                        <img src="https://www.google.com/s2/favicons?domain=${urlDomain}" style="left: 25px; width: 1.3em; height: 1.3em; vertical-align: middle;">
+                        <a href="${url}" target="_blank" style="padding-left: .25em; padding-right: 1em; text-decoration: none; cursor: pointer; color: #234da7;">
+                            ${title}&nbsp;
                         </a>
                     </div>
-                    <span style="padding-left: 31px; color: #0a375f; font-size: 11px"> · ${getTimeString(t)} · ${getTimeAgo(new Date() - t)} ·</span>
+                    <span style="padding-left: 4em; color: #0a375f; font-size: 11px"> · ${getTimeString(t)} · ${getTimeAgo(new Date() - t)} ·</span>
                 </div>`;
     }
 
