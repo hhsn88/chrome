112df9dfa86bd466e71b47c76405ad21950d82fc
Hsn <hhsn88@gmail.com>
2019-07-07 11:59:56 +0300

    [prog-trkr] fixed dual entry in reading list due to # in url

--- prog-tracker/content.js
+++ prog-tracker/content.js
@@ -2,20 +2,24 @@ chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
     if( request.msg === "save_scroll_pos" ) {
         chrome.storage.sync.get(null, (data) => {  
             if (data) { 
+                // Remove '?prg_trkr=' if present
                 const idx = document.URL.indexOf('?prg_trkr=');
                 var url = idx > 0 ? document.URL.substring(0, idx) : document.URL;
+                // Remove '#' if present
+                url = url.split('#')[0];
+                // Create / Update entry
                 data['ProgTrkr_' + url] = {
                     scroll: Math.trunc(window.scrollY),
                     title: document.title,
                     time: (new Date()).toString()
                 };
-                
+                // Save entry to storage
                 chrome.storage.sync.set(data, () => {
                     if (chrome.runtime.lastError) {
                         console.log(`Exception occured: ${chrome.runtime.lastError}`)
                     }
                 }); 
-
+                // Notify extension scripts about the update
                 chrome.runtime.sendMessage({ msg: "scroll_saved"});
             }
         });
