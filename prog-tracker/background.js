536ccd6b0675cd8fef5e6128f7c165caeffab679
hsn <hhsn88@gmail.com>
2019-07-08 22:02:38 +0300

    [prog-trkr] made reading list tab a singleton

--- prog-tracker/background.js
+++ prog-tracker/background.js
@@ -1,5 +1,15 @@
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
     if (request.msg === "open_prog_trkr") {
-        chrome.tabs.create({"url": "readingList.html"});
+        chrome.tabs.query({}, (tabs) => {
+            // Check if already opened
+            for (let i = 0; i < tabs.length; i++) {
+                if (tabs[i].url === 'chrome-extension://afccefgidedpplolmoneomagdckofajd/readingList.html') {
+                    chrome.tabs.update(tabs[i].id, {active: true});
+                    return;
+                }
+            }
+            // Tab not opened -> open it
+            chrome.tabs.create({"url": "readingList.html"});
+        })
     }
 });
